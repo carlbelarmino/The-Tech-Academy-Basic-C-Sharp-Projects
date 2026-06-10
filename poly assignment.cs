2a089fab42074d3ac59a3c5307db4292a9b6daff
Carl Belarmino <carlbelarmino_0713@yahoo.com>
2026-06-09 23:22:59 -0400

    Add files via upload

---
+++ poly assignment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PolymorphismAssignment
+{
+    public interface IQuittable
+    {
+        void Quit();
+    }
+    public class Employee: IQuittable
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void Quit()
+        {
+            Console.Writeline($"{FirstName} {LastName} has officially quit the company.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] sender)
+        {
+            IQuittable quittableEmployee= new Employee();
+            {
+                Id=101,
+                    FirstName="John",
+                    LastName="Doe"
+            };
+            quittableEmployee.Quit();
+            Console.ReadLine();
+        }
+    }
+}
+
+
