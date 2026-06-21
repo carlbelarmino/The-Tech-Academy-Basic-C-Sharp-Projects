5c570ae6fd9d16ed918d632eadc01c531689a80a
Carl Belarmino <carlbelarmino_0713@yahoo.com>
2026-06-21 10:54:46 -0400

    Add files via upload

---
+++ operators assignment/poly assignment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PolymorphismAssignment
+{
+    public interface IQuittable
+    {
+        void Quit();
+    }
+    public class Employee: IQuittable
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void Quit()
+        {
+            Console.WriteLine($"{FirstName} {LastName} has officially quit the company.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args) { 
+            //instantiate the employee object with data
+        }
+        {
+            Employee emp = new Employee() { Id = 101, FirstName = "John", LastName = "Doe" };
+        //Use Polymorphism to assign the employee to the IQuittable interface
+            
+            
+            IQuittable quittableEmployee = emp;
+        //Call the Quit method on the interface object
+            {
+                Id=101,
+                    FirstName="John",
+                    LastName="Doe"
+            };
+            quittableEmployee.Quit();//Pause the console Console.ReadLine();}
+            Console.ReadLine();
+        }
+    }
+}
+
+
