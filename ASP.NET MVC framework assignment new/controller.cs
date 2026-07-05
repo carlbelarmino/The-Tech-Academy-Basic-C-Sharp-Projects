cbf4fc62127213975e108afc4f561dbdca584a60
Carl Belarmino <carlbelarmino_0713@yahoo.com>
2026-07-05 11:32:04 -0400

    Add files via upload

---
+++ ASP.NET MVC framework assignment new/controller.cs
@@ -0,0 +1,13 @@
+﻿//Assume 'basequote' is your starting claculation amount
+decimal quotemodifier = 0
+    if(caryear<2000)
+    {
+    //Add an extra charge for older cars(e.g., plus $25)
+    quoteModifier+=25;
+}
+else if (carYear > 2015)
+{
+    //Add an extra charge for newer cars(e.g., plus $25
+    quotemodifier += 25;
+}
+decimal finalQuote = basequote + quotemodifier;
---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarlInsurancenew.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
